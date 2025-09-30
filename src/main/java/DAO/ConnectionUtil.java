c6a7394d0de1a8b46bf912b8a6721b9d558e438e
Gabriel Vitor <gabrielvcarvalho09@gmail.com>
2025-09-30 13:10:02 -0300

    Criando o básico da DAO

--- src/main/java/DAO/ConnectionUtil.java
+++ src/main/java/DAO/ConnectionUtil.java
@@ -1,7 +1,30 @@
 package DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ConnectionUtil {
     final private static String USER = "marketplaceuser";
     final private static String PASSWORD = "password";
-    final private static String URL = "";
+    final private static String URL = "jdbc:postgresql://localhost:5432/marketplace";
+
+    public static Connection openConnetion() throws SQLException {
+        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        return connection;
+    }
+
+    public static void closeConnection(Connection connection) throws SQLException {
+        if(connection != null) connection.close();
+    }
+
+    public static void closeStatement(PreparedStatement statement) throws SQLException{
+        if(statement != null) statement.close();
+    }
+
+    public static void closeAll(Connection connection, PreparedStatement statement) throws SQLException{
+        closeConnection(connection);
+        closeStatement(statement);
+    }
 }
