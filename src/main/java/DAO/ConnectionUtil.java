c84c1d62b0f590f8ae84cbc38a6a251122410988
Gabriel Vitor <gabrielvcarvalho09@gmail.com>
2025-09-30 12:46:23 -0300

    First commit

---
+++ src/main/java/DAO/ConnectionUtil.java
@@ -0,0 +1,7 @@
+package DAO;
+
+public class ConnectionUtil {
+    final private static String USER = "marketplaceuser";
+    final private static String PASSWORD = "password";
+    final private static String URL = "";
+}
---
+++ src/main/java/DAO/QueryTemplate.java
@@ -0,0 +1,7 @@
+package DAO;
+
+public class QueryTemplate {
+    private void basicCRUD(){
+
+    }
+}
