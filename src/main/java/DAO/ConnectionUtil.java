8b54e980b97fc20a89e27de756505e050e1d1ba2
Gabriel Vitor <gabrielvcarvalho09@gmail.com>
2025-09-30 17:41:02 -0300

    Read e Create

--- src/main/java/DAO/ConnectionUtil.java
+++ src/main/java/DAO/ConnectionUtil.java
@@ -1,30 +1,54 @@
 package DAO;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ConnectionUtil {
     final private static String USER = "marketplaceuser";
     final private static String PASSWORD = "password";
     final private static String URL = "jdbc:postgresql://localhost:5432/marketplace";
 
-    public static Connection openConnetion() throws SQLException {
-        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        return connection;
+    public static Connection openConnection() {
+        try{
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            return connection;
+        } catch (SQLException e) {
+            System.err.println("Erro ao abrir conexão");
+            return null;
+        }
     }
 
-    public static void closeConnection(Connection connection) throws SQLException {
-        if(connection != null) connection.close();
+    private static void closeConnection(Connection connection){
+        try{
+            if(connection != null) connection.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
-    public static void closeStatement(PreparedStatement statement) throws SQLException{
-        if(statement != null) statement.close();
+    private static void closeStatement(PreparedStatement statement){
+        try{
+            if(statement != null) statement.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
-    public static void closeAll(Connection connection, PreparedStatement statement) throws SQLException{
-        closeConnection(connection);
-        closeStatement(statement);
+    private static void closeResultSet(ResultSet resultSet){
+        try{
+            if(resultSet != null) resultSet.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static void closeAll(Connection connection, PreparedStatement statement){
+            closeStatement(statement);
+            closeConnection(connection);
+    }
+
+    public static void closeAll(Connection connection, PreparedStatement statement, ResultSet resultSet){
+            closeStatement(statement);
+            closeResultSet(resultSet);
+            closeConnection(connection);
     }
 }
--- src/main/java/DAO/QueryTemplate.java
+++ src/main/java/DAO/QueryTemplate.java
@@ -1,7 +1,77 @@
 package DAO;
 
-public class QueryTemplate {
-    private void basicCRUD(){
+import MODEL.UsuarioEntity;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class QueryTemplate {
+    public static class Usuario{
+        public void createUsuario(UsuarioEntity usuario){
+            Connection connection = ConnectionUtil.openConnection();
+            PreparedStatement pS = null;
+
+            try{
+                pS = connection.prepareStatement("INSERT INTO usuarios (nome, email, senha, role) VALUES (?, ?, ?, ?)");
+                pS.setString(1, usuario.getNome().toUpperCase());
+                pS.setString(2, usuario.getEmail().toUpperCase());
+                pS.setString(3, usuario.getSenha().toUpperCase());
+                pS.setString(4, usuario.getRole().toUpperCase());
+                pS.executeUpdate();
+            } catch (SQLException e) {
+                System.err.println("Erro ao criar Usuario");
+            } finally {
+                ConnectionUtil.closeAll(connection, pS);
+            }
+        }
+
+    public UsuarioEntity readUsuario(int id){
+            Connection connection = ConnectionUtil.openConnection();
+            PreparedStatement pS = null;
+            ResultSet rS = null;
+            try{
+                pS = connection.prepareStatement("SELECT id, nome, email, senha, role FROM usuarios WHERE id = ?");
+                pS.setInt(1, id);
+                rS = pS.executeQuery();
+
+                if(rS.next()){
+                    return new UsuarioEntity(rS.getString("nome"),
+                            rS.getString("email"),
+                            rS.getString("senha"),
+                            rS.getString("role"));
+                }else return null;
+            } catch (SQLException e) {
+                System.err.println("Erro ao ler Usuario");
+                return null;
+            } finally {
+                ConnectionUtil.closeAll(connection, pS, rS);
+            }
+        }
+
+        public UsuarioEntity readUsuario(String nome){
+            Connection connection = ConnectionUtil.openConnection();
+            PreparedStatement pS = null;
+            ResultSet rS = null;
+            try{
+                pS = connection.prepareStatement("SELECT id, nome, email, senha, role FROM usuarios WHERE nome = ?");
+                pS.setString(1, nome.toUpperCase());
+                rS = pS.executeQuery();
+
+                if(rS.next()){
+                    return new UsuarioEntity(rS.getInt("id"),
+                            rS.getString("nome"),
+                            rS.getString("email"),
+                            rS.getString("senha"),
+                            rS.getString("role"));
+                }else return null;
+            } catch (SQLException e) {
+                System.err.println("Erro ao ler Usuario");
+                return null;
+            } finally {
+                ConnectionUtil.closeAll(connection, pS, rS);
+            }
+        }
     }
 }
