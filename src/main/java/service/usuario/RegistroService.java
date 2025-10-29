ad3969a67f34272b4df07c8cb637c2b967d9488c
Gabriel Vitor <gabrielvcarvalho09@gmail.com>
2025-10-29 18:27:12 -0300

    Implementação do NullDTO no ResgistroService

--- src/main/java/service/usuario/RegistroService.java
+++ src/main/java/service/usuario/RegistroService.java
@@ -1,8 +1,10 @@
 package service.usuario;
 
+import dto.DTOUtils;
 import dto.UsuarioDTO;
 import model.UsuarioEntity;
 import repository.UserRepository;
+import service.exceptions.NullDTO;
 import service.usuario.exceptions.EmailUserAlreadyUsed;
 import service.usuario.exceptions.InvalidRole;
 import service.usuario.exceptions.NameUserAlreadyExists;
@@ -20,11 +22,17 @@ public class RegistroService {
     }
 
     public void registrar(UsuarioDTO usuarioDTO){
+        if(DTOUtils.isNull(usuarioDTO))
+            throw new NullDTO("O corpo da requisição está vazio");
+
         userRepository.salvarUsuario(new UsuarioEntity(usuarioDTO.getNome(),
                 usuarioDTO.getEmail(), usuarioDTO.getSenha(), usuarioDTO.getRole()));
     }
 
     public void verificarRegistroUsuario(UsuarioDTO usuarioDTO){
+        if(DTOUtils.isNull(usuarioDTO))
+            throw new NullDTO("O corpo da requisição está vazio");
+
         VerificacaoUtils.verificarCamposVazios(usuarioDTO.getNome(), usuarioDTO.getEmail(), usuarioDTO.getSenha());
 
         if (!roleMagenementService.isValidRole(usuarioDTO.getRole()))
